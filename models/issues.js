b98d1cb32ce96252827361cb8d8599145a2824d1
Jean Paul <69805692+freakles@users.noreply.github.com>
2020-10-31 22:29:19 +0100

    issues add?

--- models/issues.js
+++ models/issues.js
@@ -1,5 +1,7 @@
 const db = require('../db')();
 const COLLECTION = 'issues';
+const ObjectID = require('mongodb').ObjectID;
+
 
 
 module.exports = () => {
@@ -40,7 +42,7 @@ module.exports = () => {
   };
 
 //not done yet
-  const add = async (slug, title, description, status) => {
+/*  const add = async (slug, title, description, status) => {
     const project = await db.get('projects', { slug });
     const { project_id, slugName } = project;
     const issueCount = await db.count(COLLECTION);
@@ -53,7 +55,50 @@ module.exports = () => {
       comment: [],
     });
     return results.result;
-  };
+  };*/
+
+
+  const add = async (slug,title, description, status, project_id) => {
+
+    const PIPELINE = [
+      {
+        $lookup: {
+          from: 'issues',
+          localField: '_id',
+          foreignField: 'project_id',
+          as: 'issues',
+        },
+      },
+      {
+        $match: { slug },
+      },
+      {
+        $project: {
+          'issues.project_id': 0,
+        },
+      },
+    ];
+
+    const issueCount = await db.count('projects', PIPELINE);
+    
+    
+    const results = await db.add(COLLECTION,
+      {
+        issueNumber: `${slug}-${issueCount - 2}`,
+        title: title,
+        description: description,
+        status: status,
+        project_id: new ObjectID(project_id),
+        comment: [],
+      },
+      {
+        $match: { project_id },
+      });
+      
+      return results.result;
+    };
+  
+  
 
   const updateStatus = async (issueNumber, status) => {
     const PIPELINE = [
